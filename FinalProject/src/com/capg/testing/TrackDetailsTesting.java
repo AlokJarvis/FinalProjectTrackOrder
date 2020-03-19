9c20b513750cff14e770a44631b621d1d0d62393
AlokJarvis <60642135+AlokJarvis@users.noreply.github.com>
2020-03-20 01:35:31 +0530

    hello

--- FinalProject/src/com/capg/testing/TrackDetailsTesting.java
+++ FinalProject/src/com/capg/testing/TrackDetailsTesting.java
@@ -2,14 +2,23 @@ package com.capg.testing;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.capg.dao.TrackDetails;
+import com.capg.service.TrackService;
 
 import java.util.List;
 public class TrackDetailsTesting {
-	TrackDetails obj=new TrackDetails();
+	TrackDetails obj;
 	    Integer List[]= {1,2,3,4,5,6,7};
+	    
+	    @Before
+		public void setUp()
+		{
+			obj=new TrackDetails();
+		}
 		@Test
 		public void testgetId()
 		{
@@ -28,6 +37,11 @@ public class TrackDetailsTesting {
 			assertEquals("No Data Found",obj.getTrackOrderDetails(10));		
 		}
 	
+		 @After
+		    public void cleaUp()
+		    {
+		    	obj=null;
+		    }
 	}
 
 
